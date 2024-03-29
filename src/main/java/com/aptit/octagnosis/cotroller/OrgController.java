8f48b1165cd17b26370a587a289236e63e81a570
cjk <chacos@gmail.com>
2024-03-29 10:50:29 +0700

    Response 시 Http Header 에  값 쓰기 예제

--- src/main/java/com/aptit/octagnosis/cotroller/OrgController.java
+++ src/main/java/com/aptit/octagnosis/cotroller/OrgController.java
@@ -1,24 +1,31 @@
 package com.aptit.octagnosis.cotroller;
 
 import com.aptit.octagnosis.mapper.OrgMapper;
-import com.aptit.octagnosis.model.Manager;
 import com.aptit.octagnosis.model.Org;
 import com.aptit.octagnosis.model.OrgParm;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import com.aptit.octagnosis.common.CommonLib;
 @RestController
 @RequestMapping("/api")
 public class OrgController {
 
     @Autowired
     private OrgMapper OrgService;
+
+    @Autowired
+    private CommonLib Lib;
     
     Map<String, Object> Rtn = new HashMap<>();
     
@@ -43,12 +50,14 @@ public class OrgController {
     
     @PostMapping("/Org/GetOrgList")
     public Map<String, Object> GetOrgList(@RequestBody OrgParm orgParm) {
+        //public ResponseEntity<Map<String, Object>> GetOrgList(@RequestBody OrgParm orgParm) {
         
-        System.out.println("********* Limit : " +  orgParm.getPaging().getLimit());
         Map<String, Object> Rtn = new HashMap<>();
         Rtn.put("OrgTotCnt", OrgService.GetOrgListTotCnt(orgParm));
         Rtn.put("OrgList" , OrgService.GetOrgList(orgParm));
- 
+
+        //return new ResponseEntity<>(Rtn, Lib.getHeader(), HttpStatus.OK);
+        
         return Rtn;
     }
 }
